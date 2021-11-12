60b9ad034f8d1556d33ff9537629d4b1114bae12
sbx_user1051 <sbx_user1051@169.254.196.157>
2021-11-12 16:49:15 +0000

    Update requested for Proton - service/tf-svc/service-instance/service-dev

--- dev/service-dev/main.tf
+++ dev/service-dev/main.tf
@@ -1,3 +1,11 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:617296401743:service/tf-svc/service-instance/service-dev
+
+If the resource is no longer is accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
 terraform {
   required_providers {
     aws = {
@@ -5,6 +13,8 @@ terraform {
       version = "~> 3.0"
     }
   }
+  # This tells terraform to store the state file in s3 at the location
+  # s3://terraform-state-bucket/tf-os-sample/terraform.tfstate
   backend "s3" {
     bucket = "wishnick-terraform-states"
     key    = "tf-os-sample/service-dev/terraform.tfstate"
@@ -20,5 +30,15 @@ provider "aws" {
 resource "aws_ssm_parameter" "service_ssm_param" {
   name  = "service_ssm_param"
   type  = "String"
-  value = "value"
+  value = var.service_instance.inputs.ssm_parameter_value
+}
+
+resource "aws_ssm_parameter" "env_output_param" {
+  name = "env_output_param"
+  type = "String"
+  value = var.service_instance.environment.outputs.random_output
+}
+
+output "random_svc_output" {
+  value = "some_svc_output"
 }

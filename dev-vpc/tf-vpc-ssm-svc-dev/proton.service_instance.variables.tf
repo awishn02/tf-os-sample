0ae7ee130745be1f27292812848f2aa8a47a5194
sbx_user1051 <sbx_user1051@169.254.131.237>
2021-11-23 19:46:52 +0000

    Update requested for Proton - service/tf-vpc-ssm-svc/service-instance/dev

---
+++ dev-vpc/tf-vpc-ssm-svc-dev/proton.service_instance.variables.tf
@@ -0,0 +1,26 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:617296401743:service/tf-vpc-ssm-svc/service-instance/dev
+
+If the resource is no longer is accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+variable "service" {
+  type = object({
+    name = string
+  })
+}
+
+variable "service_instance" {
+  type = object({
+    name = string
+    inputs = map(string)
+    environment = object({
+      account_id = string
+      outputs = map(string)
+      name = string
+    })
+  })
+  default = null
+}

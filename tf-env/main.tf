aa5d261b213c1b86ce0ecd832833548aa0441525
sbx_user1051 <sbx_user1051@169.254.242.109>
2021-11-24 04:21:51 +0000

    Update requested for Proton - environment/tf-env

---
+++ tf-env/main.tf
@@ -0,0 +1,38 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-west-2:351419626416:environment/tf-env
+
+If the resource is no longer is accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  # This tells terraform to store the state file in s3 at the location
+  # s3://terraform-state-bucket/tf-os-sample/terraform.tfstate
+  backend "s3" {
+    bucket = "wishnick-terraform-states"
+    key    = "tf-os-sample/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_ssm_parameter" "foo_test" {
+  name  = "foo_test"
+  type  = "String"
+  value = var.environment.inputs.ssm_parameter_value
+}
+
+output "random_output" {
+  value = "some_output"
+}
